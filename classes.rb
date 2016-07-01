9186e9443fafeab283d93389067d2d78dd3c6bb8
Denis Singh <denis.singh@optus.com.au>
2016-07-01 15:29:46 +1000

    Added class ariables and class methods

--- classes.rb
+++ classes.rb
@@ -1,14 +1,20 @@
 class Name
+ @@count  = 0
  def initialize(fname, mname, lname)
   @fname = fname
   @mname = mname
   @lname = lname
+  @@count += 1
  end
  attr_reader :fname, :mname, :lname
  attr_writer :fname, :mname, :lname
  def to_s
   print (@fname + " " + @mname + " " + @lname + "\n")
  end
+
+ def self.count
+  @@count
+ end 
 end
 
 aName = Name.new("Denis", "Shalendra","Singh")
@@ -18,6 +24,8 @@ print("\n")
 print aName.mname
 print("\n")
 print aName.lname
+print("\n")
+print("Count is ", Name.count)
 print("\n\n")
 aName.fname="Jahaan"
 aName.mname="Rajendra"
@@ -28,4 +36,7 @@ print("\n")
 print aName.mname
 print("\n")
 print aName.lname
-print("\n\n")
+aName = Name.new("Ram","Shankar","Kumar")
+print("\n")
+print("Count is ", Name.count)
+print("\n")
